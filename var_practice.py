1b0ef7d580e94382429c447eee7cac2d48017da3
SanaL22 <354231368@educ.dpcdsb.org>
2026-02-12 12:33:32 -0500

    var_practice.py

---
+++ var_practice.py
@@ -0,0 +1,25 @@
+"""
+Story by Sana Laone
+"""
+
+# Input
+place = input("Please enter a place:  ")
+adjective = input("Please enter a adjective:  ")
+noun = input("Please enter a noun: ")
+name = input("Please enter a name:  ")
+pronoun = input("Please enter a pronoun:  ")
+verb =  input("Please enter a verb: ")
+destination = input("Please enter a destination: ")
+
+
+
+
+# Processing
+# Output
+print(f"Last summer me and my frined {name} decided to go to {destination}, which is located in {place}.")
+print(f"My sister has been to {place} befor, so {pronoun} told us all the place we should go to.")
+print(f"It was supper hot in {place}, but that only ment we could eat more gelato.")
+print(f"On the second day there we decided to go to the {noun}.")
+print(f"it was so {adjective} to see it in person")
+print(f"Every day at night me and {name} would {verb} around the area near our hotel.")
+print(f"It was so fun experiencing italian culture, I can't wait to go back.")
